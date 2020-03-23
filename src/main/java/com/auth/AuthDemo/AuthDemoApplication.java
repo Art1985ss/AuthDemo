9e93332388ee1d672faa0a78d9963f7388e518c7
Aigars Strods <strods.aigars@gmail.com>
2020-03-23 15:04:55 +0200

    Added line for testing

--- src/main/java/com/auth/AuthDemo/AuthDemoApplication.java
+++ src/main/java/com/auth/AuthDemo/AuthDemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.awt.*;
-
+//TestLine
 @SpringBootApplication
 public class AuthDemoApplication {
 	@Autowired
