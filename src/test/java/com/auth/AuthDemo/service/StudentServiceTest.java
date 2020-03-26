127980648a363e7ba6008dea3abdd5a5401d1070
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-03-27 01:33:59 +0200

    Created empty tests for Services. Issue #13

---
+++ src/test/java/com/auth/AuthDemo/service/StudentServiceTest.java
@@ -0,0 +1,42 @@
+package com.auth.AuthDemo.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByName() {
+    }
+}
