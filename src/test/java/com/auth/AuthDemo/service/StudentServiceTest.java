41ee2b54f1466b6954250f9d057fb07c94cf93d3
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-03-27 13:58:58 +0200

    Issue #13. Added tests for StudentService.

--- src/test/java/com/auth/AuthDemo/service/StudentServiceTest.java
+++ src/test/java/com/auth/AuthDemo/service/StudentServiceTest.java
@@ -1,15 +1,35 @@
 package com.auth.AuthDemo.service;
 
+import com.auth.AuthDemo.entity.User;
+import com.auth.AuthDemo.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class StudentServiceTest {
 
+    @Mock
+    private UserRepository userRepository;
+
+    private User user;
+    private StudentService studentService;
+    private final long expectedId = 12L;
+
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        user = new User();
+        user.setId(expectedId);
+        studentService = new StudentService(userRepository);
     }
 
     @AfterEach
@@ -18,18 +38,28 @@ class StudentServiceTest {
 
     @Test
     void create() {
+        when(userRepository.save(user)).thenReturn(user);
+        assertEquals(expectedId, studentService.create(user));
     }
 
     @Test
     void update() {
+        when(userRepository.save(user)).thenReturn(user);
+        assertEquals(expectedId, studentService.create(user));
     }
 
     @Test
     void findById() {
+        final Optional<User> result = Optional.of(user);
+
+        when(userRepository.findById(expectedId)).thenReturn(result);
+        assertEquals(user, studentService.findById(expectedId));
     }
 
     @Test
     void deleteById() {
+        studentService.deleteById(expectedId);
+        verify(userRepository).deleteById(expectedId);
     }
 
     @Test
