c07d6ba2445f4caa9445c1bfa9d7e3e32c3c345f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-03-27 12:45:47 +0200

    Issue #13. Added tests for QuestionService.

--- src/test/java/com/auth/AuthDemo/service/QuestionServiceTest.java
+++ src/test/java/com/auth/AuthDemo/service/QuestionServiceTest.java
@@ -1,15 +1,38 @@
 package com.auth.AuthDemo.service;
 
+import com.auth.AuthDemo.entity.Question;
+import com.auth.AuthDemo.repository.QuestionRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 class QuestionServiceTest {
+
+    @Mock
+    private QuestionRepository questionRepository;
+
+    private QuestionService questionService;
+    private Question question;
+    private final long expectedId = 12L;
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        question = new Question();
+        question.setId(expectedId);
+        questionService = new QuestionService(questionRepository);
     }
 
     @AfterEach
@@ -18,22 +41,33 @@ class QuestionServiceTest {
 
     @Test
     void create() {
+        when(questionRepository.save(question)).thenReturn(question);
+        assertEquals(expectedId, questionService.create(question));
     }
 
     @Test
     void update() {
+        when(questionRepository.save(question)).thenReturn(question);
+        assertEquals(expectedId, questionService.create(question));
     }
 
     @Test
     void findById() {
+        final Optional<Question> result = Optional.of(question);
+
+        when(questionRepository.findById(expectedId)).thenReturn(result);
+        assertEquals(question, questionService.findById(expectedId));
     }
 
     @Test
     void deleteById() {
+        questionService.deleteById(expectedId);
+        verify(questionRepository).deleteById(expectedId);
     }
 
     @Test
     void findAll() {
+
     }
 
     @Test
