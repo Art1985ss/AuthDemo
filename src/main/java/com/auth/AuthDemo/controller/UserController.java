85b9b1023b9166deee2b95d38a9816a77f7db3f6
Art1985 <art_1985@inbox.lv>
2020-03-31 17:50:21 +0300

    RbZt_remove_v1 Added user registration controllers Added remove controller for test Added remove controller for question

--- src/main/java/com/auth/AuthDemo/controller/UserController.java
+++ src/main/java/com/auth/AuthDemo/controller/UserController.java
@@ -9,10 +9,12 @@ import com.auth.AuthDemo.service.UserService;
 import com.auth.AuthDemo.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.math.BigDecimal;
 import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,16 +35,32 @@ public class UserController {
         User user = userService.findByName(principal.getName());
         user.updateScore();
 
-        List<DtoTestKC> allDtoTests = testService.findAll().stream().map(t -> DtoConverter.toDto(t)).collect(Collectors.toList());
-
-        System.out.println("user controller");
-        System.out.println(user);
+        List<DtoTestKC> allDtoTests = testService.findAll().stream().map(DtoConverter::toDto).collect(Collectors.toList());
         DtoUser dtoUser = DtoConverter.toDto(user);
         List<DtoTestKC> dtoTestKCList =  user.getUserTests().stream().map(u-> DtoConverter.toDto(user, u.getTestKC())).collect(Collectors.toList());//testService.findAll().stream().map(DtoConverter::toDto).collect(Collectors.toList());
         modelAndView.addObject("user", dtoUser);
         modelAndView.addObject("test", dtoTestKCList);
         modelAndView.addObject("allDtoTests", allDtoTests);
         modelAndView.setViewName("user");
+        return modelAndView;
+    }
+
+    @GetMapping("/registration")
+    public ModelAndView newUserForm(){
+        ModelAndView modelAndView = new ModelAndView("");
+        DtoUser dtoUser = new DtoUser();
+        dtoUser.setName("");
+        dtoUser.setPassword("");
+        dtoUser.setScore(BigDecimal.ZERO);
+        modelAndView.addObject("dtoUser", dtoUser);
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView create(@RequestBody DtoUser dtoUser){
+        ModelAndView modelAndView = new ModelAndView("redirect:/");
+        User user = DtoConverter.fromDto(dtoUser);
+        userService.create(user);
         return modelAndView;
     }
 
