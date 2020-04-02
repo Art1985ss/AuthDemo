77e273f413d57cd7da0c5a6b5d32cc902d8311f4
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-04-02 21:51:06 +0300

    Issue #14. Added UserControllerTest

--- src/test/java/com/auth/AuthDemo/controller/UserControllerTest.java
+++ src/test/java/com/auth/AuthDemo/controller/UserControllerTest.java
@@ -1,24 +1,32 @@
 package com.auth.AuthDemo.controller;
 
+import com.auth.AuthDemo.dto.DtoUser;
 import com.auth.AuthDemo.entity.TestKC;
 import com.auth.AuthDemo.entity.User;
+import com.auth.AuthDemo.entity.UserTest;
 import com.auth.AuthDemo.service.QuestionService;
 import com.auth.AuthDemo.service.ScoreCalculationService;
 import com.auth.AuthDemo.service.TestService;
 import com.auth.AuthDemo.service.UserService;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.ModelAndViewAssert.assertModelAttributeAvailable;
+import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
 
 class UserControllerTest {
+
+    final private Long testId = 16l;
+    final private String userName = "John Doe";
 
     @InjectMocks
     private UserController userController;
@@ -27,14 +35,8 @@ class UserControllerTest {
     @Mock
     private UserService userService;
     @Mock
-    private QuestionService questionService;
-    @Mock
-    private ScoreCalculationService scoreCalculationService;
-    @Mock
     private Principal principal;
 
-    final private Long testId = 16l;
-    final private String userName = "John Doe";
     private TestKC testKC;
     private User user;
 
@@ -43,36 +45,36 @@ class UserControllerTest {
         MockitoAnnotations.initMocks(this);
         user = new User();
         user.setId(1L);
+        user.setUserTests(new HashSet<UserTest>());
         testKC = new TestKC();
-    }
-
-    @AfterEach
-    void tearDown() {
     }
 
     @Test
     void getTestDate() {
-        when(testService.findById(testId)).thenReturn(testKC);
         when(principal.getName()).thenReturn(userName);
+        when(testService.findAll()).thenReturn(new ArrayList<TestKC>() {{
+            add(testKC);
+        }});
+        when(userService.findByName(userName)).thenReturn(user);
+
+        final ModelAndView actual = userController.getTestDate(principal);
+
+        assertViewName(actual, "user");
+        assertModelAttributeAvailable(actual, "allDtoTests");
+        assertModelAttributeAvailable(actual, "test");
+        assertModelAttributeAvailable(actual, "user");
     }
 
     @Test
     void newUserForm() {
+        final ModelAndView actual = userController.newUserForm();
+        assertViewName(actual, "userRegistration");
+        assertModelAttributeAvailable(actual, "dtoUser");
     }
 
     @Test
     void create() {
-    }
-
-    @Test
-    void testGetTestDate() {
-    }
-
-    @Test
-    void testNewUserForm() {
-    }
-
-    @Test
-    void testCreate() {
+        final ModelAndView actual = userController.create(new DtoUser());
+        assertViewName(actual, "redirect:/home");
     }
 }
