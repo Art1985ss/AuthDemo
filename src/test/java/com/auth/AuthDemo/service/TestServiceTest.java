9ed530acf21a0ee9bae78d38cca4d9a66bae5cf4
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-04-02 23:35:27 +0300

    Issue #14. Added tests for create, update, findById and deleteById for testService

--- src/test/java/com/auth/AuthDemo/service/TestServiceTest.java
+++ src/test/java/com/auth/AuthDemo/service/TestServiceTest.java
@@ -1,15 +1,40 @@
 package com.auth.AuthDemo.service;
 
+import com.auth.AuthDemo.entity.TestKC;
+import com.auth.AuthDemo.repository.TestRepository;
+import com.auth.AuthDemo.service.validation.test.TestValidationService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 class TestServiceTest {
+    @InjectMocks
+    private TestService testService;
+
+    @Mock
+    private TestRepository testRepository;
+    @Mock
+    private TestValidationService testValidationService;
+
+    private TestKC testKC;
+    private final Long testId = 12l;
 
     @BeforeEach
     void setUp() {
+        initMocks(this);
+
+        testKC = new TestKC();
+        testKC.setId(testId);
     }
 
     @AfterEach
@@ -18,18 +43,39 @@ class TestServiceTest {
 
     @Test
     void create() {
+        when(testRepository.save(testKC)).thenReturn(testKC);
+
+        final Long actualId = testService.create(testKC);
+
+        verify(testRepository).save(testKC);
+        verify(testValidationService).validate(testKC);
+        assertEquals(testId, actualId);
     }
 
     @Test
     void update() {
+        when(testRepository.save(testKC)).thenReturn(testKC);
+
+        final Long actualId = testService.update(testKC);
+
+        verify(testRepository).save(testKC);
+        assertEquals(testId, actualId);
     }
 
     @Test
     void findById() {
+        when(testRepository.findById(testId)).thenReturn(Optional.of(testKC));
+
+        final TestKC actual = testService.findById(testId);
+
+        verify(testRepository).findById(testId);
+        assertEquals(testKC, actual);
     }
 
     @Test
     void deleteById() {
+        testService.deleteById(testId);
+        verify(testRepository).deleteById(testId);
     }
 
     @Test
