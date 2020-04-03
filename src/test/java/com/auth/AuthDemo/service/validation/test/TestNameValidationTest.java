792b1afd5636dbc792343a6f6d5abb813068ad2b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-04-03 12:20:49 +0300

    Issue #14. Added validate() for TestNameValidationTest

--- src/test/java/com/auth/AuthDemo/service/validation/test/TestNameValidationTest.java
+++ src/test/java/com/auth/AuthDemo/service/validation/test/TestNameValidationTest.java
@@ -2,15 +2,15 @@ package com.auth.AuthDemo.service.validation.test;
 
 import com.auth.AuthDemo.entity.TestKC;
 import com.auth.AuthDemo.repository.TestRepository;
+import com.auth.AuthDemo.service.validation.ValidationException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 class TestNameValidationTest {
@@ -18,7 +18,7 @@ class TestNameValidationTest {
     @Mock
     private TestRepository testRepository;
     @InjectMocks
-    private TestNameValidation validation;
+    private TestNameValidation testNameValidation;
 
     @BeforeEach
     void setUp() {
@@ -32,13 +32,16 @@ class TestNameValidationTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/test_name_validation.csv", numLinesToSkip = 1)
-    void validate(int id, String name) {
-        TestKC test = new TestKC();
-        test.setId((long) id);
+    void validate(final long id, final String name, final String expectedException) {
+        final TestKC test = new TestKC();
+        test.setId(id);
         test.setName(name);
-    }
 
-    @Test
-    void checkMaxLength() {
+        try {
+            testNameValidation.validate(test);
+            assertEquals(expectedException, "");
+        } catch (ValidationException e) {
+            assertEquals(expectedException, e.getMessage());
+        }
     }
 }
